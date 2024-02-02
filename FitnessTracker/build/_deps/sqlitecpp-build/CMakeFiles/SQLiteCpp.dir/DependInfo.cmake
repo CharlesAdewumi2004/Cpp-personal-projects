54be462d7e7f6110c03e52d3f3e310c6ff3141ce
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2024-02-02 12:42:36 +0000

    forsn

---
+++ FitnessTracker/build/_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Backup.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Backup.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Backup.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Column.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Column.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Column.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Database.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Database.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Database.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Exception.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Exception.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Exception.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Savepoint.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Savepoint.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Savepoint.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Statement.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Statement.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Statement.cpp.o.d"
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-src/src/Transaction.cpp" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Transaction.cpp.o" "gcc" "_deps/sqlitecpp-build/CMakeFiles/SQLiteCpp.dir/src/Transaction.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/charlie/Cpp-personal-projects/FitnessTracker/build/_deps/sqlitecpp-build/sqlite3/CMakeFiles/sqlite3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FitnessTracker/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <stdexcept>
+
+using namespace std;
+
+class DatabaseManger
+{
+private:
+    
+public:
+    DatabaseManger(/* args */);
+    ~DatabaseManger();
+};
+
+DatabaseManger::DatabaseManger(/* args */)
+{
+}
+
+DatabaseManger::~DatabaseManger()
+{
+}
+
+
+enum class Gender {
+    male,
+    female,
+    nonbinary
+};
+
+class User
+{
+private:
+    int UserID, age;
+    float height, weight;
+    string username, password;
+    Gender gender;
+public:
+    User(int const UserID, int age, float height, float weight, string username, string password, Gender gender): UserID(UserID), 
+    age(age), height(height), username(move(username)), password(move(password)), gender(gender){}
+    ~User() = default;
+    int getUserID()const{return UserID;}
+    int getAge()const{return age;}
+    float getHeight()const{return height;}
+    float getWeight()const{return weight;}
+    string getUserName()const{return username;}
+    string getPassword()const{return password;}
+    void setHeight();
+    void setWeight();
+
+};
+
+class UserManager
+{
+public:
+    UserManager() = default;
+    ~UserManager() = default;
+    static User logIn(string username, string password){
+
+    }
+    static User registerUser(string username, string password){
+
+    }
+};
+
+
+
+int main(){
+
+    return 0;
+}
+
